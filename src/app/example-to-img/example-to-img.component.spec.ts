c64afd2603dda6fe730c722ad2d089d271c0512a
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2024-06-14 18:51:48 -0700

    Set indent width to 4

--- src/app/example-to-img/example-to-img.component.spec.ts
+++ src/app/example-to-img/example-to-img.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ExampleToImgComponent } from './example-to-img.component';
 
 describe('ExampleToImgComponent', () => {
-  let component: ExampleToImgComponent;
-  let fixture: ComponentFixture<ExampleToImgComponent>;
+    let component: ExampleToImgComponent;
+    let fixture: ComponentFixture<ExampleToImgComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ExampleToImgComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [ExampleToImgComponent]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(ExampleToImgComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(ExampleToImgComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/example-to-img/example-to-img.component.ts
+++ src/app/example-to-img/example-to-img.component.ts
@@ -2,24 +2,24 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import * as htmlToImage from 'html-to-image';
 
 @Component({
-  selector: 'app-example-to-img',
-  standalone: true,
-  imports: [],
-  templateUrl: './example-to-img.component.html',
-  styleUrl: './example-to-img.component.css'
+    selector: 'app-example-to-img',
+    standalone: true,
+    imports: [],
+    templateUrl: './example-to-img.component.html',
+    styleUrl: './example-to-img.component.css'
 })
 export class ExampleToImgComponent {
   @ViewChild('targetToDownload', { static: false }) content!: ElementRef;
 
   takeScreenshot() {
-    const element = this.content.nativeElement;
+      const element = this.content.nativeElement;
 
-    htmlToImage.toPng(element)
-      .then(function (dataUrl) {
-        const link = document.createElement('a');
-        link.href = dataUrl;
-        link.download = 'canvas-image.png';
-        link.click();
-      });
+      htmlToImage.toPng(element)
+          .then(function (dataUrl) {
+              const link = document.createElement('a');
+              link.href = dataUrl;
+              link.download = 'canvas-image.png';
+              link.click();
+          });
   }
 }
