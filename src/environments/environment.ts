05b43655a1a34eea5e5b120f449efc13df4e7460
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2024-06-28 21:30:46 -0700

    Remove replaceEnv script. Add prod environment replacing.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 import { IEnvironment } from './environment.interface';
 
 // Do not put real values for keys in this file.
-// During development, Put real values in the environment.development.ts
-// file and keep that file out of version control.
+// During development, Put real values in
+// environment.development.ts or environment.prod.ts
 
 export const environment: IEnvironment = {
     production: true,
-    GOOGLE_MAPS_API: 'undefined',
+    GOOGLE_MAPS_API: '',
 };
